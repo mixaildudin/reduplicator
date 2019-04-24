029649217145087cbd3c72dfcdc8dddd2b634a7a
Mikhail Dudin <mdudin@fogsoft.ru>
2019-04-24 21:24:39 +0300

    Added a new advanced stress-based reduplication algorithm

--- src/reduplicator.ts
+++ src/reduplicator.ts
@@ -2,6 +2,11 @@ import * as fs from 'fs';
 
 declare type StressDictionary = { [word: string]: number };
 
+interface WordStressInfo {
+    letterNumber: number,
+    syllableNumber: number
+}
+
 export default class Reduplicator {
     private readonly minWordLength = 3;
     private readonly vowels: Set<string>;
@@ -55,11 +60,47 @@ export default class Reduplicator {
         const wordLetters = word.split('');
 
         const syllableCount = this.getSyllableCount(wordLetters);
+        if (syllableCount === 0) {
+            return null;
+        }
+
         if (syllableCount === 1) {
             return this.singleSyllableWordPrefix + word;
         }
 
-        const knownStressedLetterIdx = this.dict[word];
+        const stressInfo = this.getStressInfo(word);
+
+        // слова из двух слогов и слова с ударением на первый слог редуплицируем дефолтным алгоритмом.
+        // от слов с 3 и менее слогами отрезается слишком много, поэтому слова с неизвестным ударением и в которых менее трех слогов
+        // тоже обрабатываем дефолтным алгоритмом
+        if (syllableCount === 2 || (stressInfo && stressInfo.syllableNumber === 1) || (!stressInfo && syllableCount <= 3)) {
+            return this.reduplicateSimply(wordLetters, stressInfo);
+        } else {
+            return this.reduplicateAdvanced(wordLetters, stressInfo);
+        }
+    }
+
+    private getSyllableCount(wordLetters: string[]): number {
+        return wordLetters.filter(x => this.vowels.has(x)).length;
+    }
+
+    private getVowelPair(vowel: string, forStressedVowel: boolean): string {
+        return (forStressedVowel
+            ? this.stressedVowelPairs[vowel] 
+            : this.defaultVowelPairs[vowel]);
+    }
+
+    private getStressInfo(word: string): WordStressInfo {
+        const letterNumber = this.dict[word] || this.dict[word.replace(/ё/g, 'е')];
+        if (letterNumber == null) return null;
+
+        const syllableNumber = this.getSyllableCount(word.substring(0, letterNumber + 1).split(''));
+
+        return { letterNumber, syllableNumber }
+    }
+
+    private reduplicateSimply(wordLetters: string[], stressInfo: WordStressInfo): string {
+        const knownStressedLetterIdx = stressInfo && stressInfo.letterNumber;
 
         for (let i = 0; i < wordLetters.length - 1; i++) {
             const curLetter = wordLetters[i];
@@ -68,7 +109,7 @@ export default class Reduplicator {
             if (this.vowels.has(curLetter)) {
                 // если буква ударная или за ней идет согласная, редуплицируем
                 if (knownStressedLetterIdx === i || this.consonants.has(nextLetter)) {
-                    return this.prefix + this.getVowelPair(word, curLetter, i, knownStressedLetterIdx) + word.substring(i + 1);
+                    return this.prefix + this.getVowelPair(curLetter, knownStressedLetterIdx == null || knownStressedLetterIdx === i ) + wordLetters.slice(i + 1).join('');
                 }
             }
         }
@@ -76,13 +117,24 @@ export default class Reduplicator {
         return null;
     }
 
-    private getSyllableCount(wordLetters: string[]): number {
-        return wordLetters.filter(x => this.vowels.has(x)).length;
-    }
+    private reduplicateAdvanced(wordLetters: string[], stressInfo: WordStressInfo): string {
+        const prefixSyllableCount = 2;
 
-    private getVowelPair(word: string, vowel: string, vowelIdx: number, knownStressedLetterIdx: number): string {
-        return (knownStressedLetterIdx === undefined || knownStressedLetterIdx === vowelIdx 
-            ? this.stressedVowelPairs[vowel] 
-            : this.defaultVowelPairs[vowel]);
+        let letterNumber = 0, syllableNumber = 0, vowel: string = null;
+
+        // найдем гласную букву, соответствующую второму слогу
+        for (letterNumber = 0; letterNumber < wordLetters.length; letterNumber++) {
+            if (this.vowels.has(wordLetters[letterNumber])) {
+                syllableNumber++;
+                vowel = wordLetters[letterNumber];
+            }
+
+            if (syllableNumber === prefixSyllableCount) {
+                break;
+            }
+        }
+
+        const pairVowel = this.getVowelPair(vowel, !stressInfo || stressInfo.syllableNumber === prefixSyllableCount);
+        return this.prefix + pairVowel + wordLetters.slice(letterNumber + 1).join('');
     }
 }
--- src/test/tests.ts
+++ src/test/tests.ts
@@ -9,12 +9,37 @@ describe('Reduplicator', () => {
         it('should correctly reduplicate words from the dictionary', () => {            
             expect(r.reduplicate('окно')).to.equal('хуекно');
             expect(r.reduplicate('бомба')).to.equal('хуёмба');
-            expect(r.reduplicate('пятерочка')).to.equal('хуетерочка');
+            expect(r.reduplicate('пятёрочка')).to.equal('хуёрочка');
             expect(r.reduplicate('бомба')).to.equal('хуёмба');
             expect(r.reduplicate('яишница')).to.equal('хуишница');
             expect(r.reduplicate('поезд')).to.equal('хуёезд');
             expect(r.reduplicate('поезда')).to.equal('хуезда');
             expect(r.reduplicate('баунти')).to.equal('хуяунти');
+
+            expect(r.reduplicate('горка')).to.equal('хуёрка');
+            expect(r.reduplicate('доска')).to.equal('хуеска');
+
+            expect(r.reduplicate('засветло')).to.equal('хуясветло');
+            expect(r.reduplicate('творожник')).to.equal('хуёжник');
+            expect(r.reduplicate('мобильный')).to.equal('хуильный');
+            expect(r.reduplicate('татары')).to.equal('хуяры');
+            expect(r.reduplicate('борода')).to.equal('хуеда');
+            expect(r.reduplicate('бородка')).to.equal('хуёдка');
+
+            expect(r.reduplicate('выставивший')).to.equal('хуиставивший');
+            expect(r.reduplicate('студенческий')).to.equal('хуенческий');
+            expect(r.reduplicate('казачество')).to.equal('хуячество');
+            expect(r.reduplicate('холодильник')).to.equal('хуедильник');
+            expect(r.reduplicate('переворот')).to.equal('хуеворот');
+            expect(r.reduplicate('переболеть')).to.equal('хуеболеть');
+
+            expect(r.reduplicate('выпотрошила')).to.equal('хуипотрошила');
+            expect(r.reduplicate('дизайнерские')).to.equal('хуяйнерские');
+            expect(r.reduplicate('положение')).to.equal('хуежение');
+            expect(r.reduplicate('перевороты')).to.equal('хуевороты');
+            expect(r.reduplicate('полуфабрикат')).to.equal('хуефабрикат');
+            
+            expect(r.reduplicate('объяснительная')).to.equal('хуеснительная');
         });
 
         it('should correctly reduplicate words with capital letters', () => {
@@ -25,6 +50,8 @@ describe('Reduplicator', () => {
         it('should correctly reduplicate single-syllable words', () => {            
             expect(r.reduplicate('кот')).to.equal('хуекот');
             expect(r.reduplicate('пёс')).to.equal('хуепёс');
+            expect(r.reduplicate('караганда')).to.equal('хуяганда');
+            expect(r.reduplicate('балтика')).to.equal('хуялтика');
         });
 
         it('should correctly reduplicate words not found in the dictionary', () => {            
